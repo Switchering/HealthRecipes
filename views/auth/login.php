a610a6bdee9f61da03fe0909219b7e6ab5e51499
Switchering <maksser@outlook.com>
2018-12-23 15:28:19 +0300

    Оптимизация кода

--- views/auth/login.php
+++ views/auth/login.php
@@ -39,13 +39,16 @@ $this->params['breadcrumbs'][] = $this->title;
           <?php ActiveForm::end(); ?>
       </div>
       <div class="col-md-2">
-        <script type="text/javascript" src="https://vk.com/js/api/openapi.js?159"></script>
-        <script type="text/javascript">VK.init({apiId: 6748665});</script>
-        <!-- VK Widget -->
-        <div id="vk_auth"></div>
-        <script type="text/javascript">
-            VK.Widgets.Auth("vk_auth", {"authUrl":"/auth/login-vk"});
-        </script>
+        <script type="text/javascript" src="https://vk.com/js/api/openapi.js?160"></script>
+<script type="text/javascript">
+  VK.init({apiId: 6748665});
+</script>
+
+<!-- VK Widget -->
+<div id="vk_auth"></div>
+<script type="text/javascript">
+  VK.Widgets.Auth("vk_auth", {"authUrl":"/dev/Login"});
+</script>
       </div>
     </div>
 </div>
